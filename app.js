598f2fb4f01fd5befed442f6253336d1e38572a3
Bruno Oliveira <brunoserrasilva@gmail.com>
2024-11-06 06:25:47 -0300

    Segundo commit do lab beanstalk pipeline v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Olá, Rodou uma Pipeline no codepipeline da AWS! -V1'));
+app.get('/', (req, res) => res.send('Olá, Rodou uma Pipeline no codepipeline da AWS! -V2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
